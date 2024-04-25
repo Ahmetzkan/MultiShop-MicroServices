24c07ad122afe20026ba20f0198e863f8e43ac3c
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2024-04-25 22:04:39 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductDetailsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Services.ProductDetailServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly IProductDetailService _ProductDetailsService;
+
+        public ProductDetailsController(IProductDetailService ProductDetailsService)
+        {
+            _ProductDetailsService = ProductDetailsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductDetailAsync()
+        {
+            var values = await _ProductDetailsService.GetAllProductDetailAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdProductDetailAsync(string id)
+        {
+            var values = await _ProductDetailsService.GetByIdProductDetailAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductDetailAsync(CreateProductDetailDto createProductDetailsDto)
+        {
+            await _ProductDetailsService.CreateProductDetailAsync(createProductDetailsDto);
+            return Ok("Product Details added succesfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailsDto)
+        {
+            await _ProductDetailsService.UpdateProductDetailAsync(updateProductDetailsDto);
+            return Ok("Product Details added succesfully");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductDetailAsync(string id)
+        {
+            await _ProductDetailsService.DeleteProductDetailAsync(id);
+            return Ok("Product Details deleted succesfully");
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+
+        private readonly IProductImageService _ProductImageService;
+
+        public ProductImagesController(IProductImageService ProductImageService)
+        {
+            _ProductImageService = ProductImageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductImageAsync()
+        {
+            var values = await _ProductImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdProductImageAsync(string id)
+        {
+            var values = await _ProductImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImageAsync(CreateProductImageDto createProductImageDto)
+        {
+            await _ProductImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Product Image added succesfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto)
+        {
+            await _ProductImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Product Image updated succesfully");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductImageAsync(string id)
+        {
+            await _ProductImageService.DeleteProductImageAsync(id);
+            return Ok("Product Image deleted succesfully");
+        }
+    }
+}
